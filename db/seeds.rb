564a0ee8995349dd8f36fdf845ed837002c158b7
Westley Andrew <westleyandrew@hotmail.com>
2019-08-16 16:54:01 +0100

    last push of the day?

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,14 @@
-Ingredient.create(name: "vodka")
-Ingredient.create(name: "rum")
-Ingredient.create(name: "moon juice")
+require 'open-uri'
+require 'json'
+
+Ingredient.destroy_all
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialized = open(url).read
+ingredients = JSON.parse(ingredients_serialized)
+
+drink_list = ingredients["drinks"]
+
+drink_list.each do |drink|
+  Ingredient.create(name: drink["strIngredient1"])
+end
