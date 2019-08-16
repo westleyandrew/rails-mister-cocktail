8be02bbaac7c9f525a50db54fdf6473bd933abe3
Westley Andrew <westleyandrew@hotmail.com>
2019-08-16 11:21:30 +0100

    root bits

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # get 'cocktails', to: 'cocktails#index'
-  # get 'cocktails/new', to: 'cocktails#new'
-  # get 'cocktails/:id', to: 'cocktails#show'
-  # post 'cocktails/', to: 'cocktails#create', as: 'cocktail'
-
+  root to: "cocktails#index"
   resources :cocktails do
     resources :doses, only: [:new, :create]
   end
