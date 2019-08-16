5465188b99a8068131a4874bfb87306c5532ead0
Westley Andrew <westleyandrew@hotmail.com>
2019-08-16 13:46:47 +0100

    delete working

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -22,6 +22,11 @@ class CocktailsController < ApplicationController
     end
   end
 
+  def destroy
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+
   private
 
   def cocktail_params
