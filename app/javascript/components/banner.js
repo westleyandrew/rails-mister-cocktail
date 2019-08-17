d9476e893d33be947bf89b58b6048f6ac9699c02
Westley Andrew <westleyandrew@hotmail.com>
2019-08-17 21:01:54 +0100

    drink responsibly

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["Please drink responsibly - 2 much booze + I'll tell your Mum."],
+    strings: ["Please drink responsibly - 2 much + I'll tell your Mum"],
     typeSpeed: 50,
     loop: true
   });
